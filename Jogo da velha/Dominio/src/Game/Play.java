31e1881fcad6a622517a1f327f70be93c2bf1450
Danielly <dani.contatoxx@gmail.com>
2025-05-08 21:17:28 -0300

    Add game-ending conditions and IntelliJ configuration files
    Added logic to check for game-ending conditions, including a winner or a tie in the Tic-Tac-Toe game. Additionally, included IntelliJ project configuration files and updated the Play.java class to ensure the game terminates correctly.

--- Jogo da velha/Dominio/src/Game/Play.java
+++ Jogo da velha/Dominio/src/Game/Play.java
@@ -7,6 +7,10 @@ public class Play {
     Scanner scanner = new Scanner(System.in);
     String[][] board = new String[3][3];
     private int player; // 0 representa circulo e X o x mesmo KKKKK
+    int stop = 1;
+    int cont = 1;
+
+
 
     public Play() {
         for (int i = 0; i < 3; i++) {
@@ -21,8 +25,9 @@ public class Play {
             display();
             checkPlayer();
             mood();
+            checkStart();
 
-        }while (true);
+        }while (stop!=0);
     }
 
     public void display() {
@@ -51,7 +56,7 @@ public class Play {
         System.out.println("Digite uma coluna: ");
         column = scanner.nextInt();
 
-        if (line >= 0 && line < board.length && column >= 0 && column < board.length && board [line][column].equals("")) {
+        if (line >= 0 && line < board.length && column >= 0 && column < board.length && board [line][column]==("")) {
             fillInBoard(line, column);
             break;
             } else {
@@ -69,9 +74,63 @@ public class Play {
             board[line][column] = "X";
         }
 
-
-
         return board[line][column];
     }
+
+    public void checkStart() {
+       if (cont < 9){
+                    //verificar linhas:
+                    for (int i = 0; i < 3; i++) {
+                        if ( board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0]!=("")) {
+                            if (player==0){
+                                System.out.println("O 'O' ganhou");
+                                stop=0;
+                            }else{
+                                System.out.println("O 'X' ganhou");
+                                stop=0;
+                            }
+                        }
+                    }
+                                        //verificar colunas:
+                               for (int j = 0; j < 3; j++) {
+                                   if ( board[0][j] == board[1][j] && board[0][j] == board[2][j] && board[0][j]!=("")) {
+                                       if (player==0){
+                                           display();
+                                           System.out.println("O 'O' ganhou");
+                                           stop=0;
+                                       }else{
+                                           display();
+                                           System.out.println("O 'X' ganhou");
+                                           stop=0;
+                                       }
+                                   }
+                               }
+                    //vericiar diagonal:
+
+                if ((board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0]!=("")) ||
+                    (board[2][0] == board[1][1] && board[2][0] == board[0][2] && board[0][2]!=(""))) {
+                    if (player==0){
+                        display();
+                        System.out.println("O 'O' ganhou");
+                        stop=0;
+                    }else{
+                        display();
+                        System.out.println("O 'X' ganhou");
+                        stop=0;
+                    }
+                }
+
+
+
+       }else{
+            display();
+            System.out.println("Deu velha");
+            stop = 0;
+       }
+
+        cont++;
+    }
+
+
 
 }
